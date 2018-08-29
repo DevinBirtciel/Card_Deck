130f112f9907cb07b00a59eac08b780447c36352
devinb <dmb1@email.arizona.edu>
2018-08-29 16:40:16 -0700

    some pojos

---
+++ src/Card.java
@@ -0,0 +1,28 @@
+
+public class Card {
+	private Suit suit;
+	private Rank rank;
+	
+	private Card(Suit suit, Rank rank) {
+		this.suit = suit;
+		this.rank = rank;
+	}
+
+	public Suit getSuit() {
+		return suit;
+	}
+
+	public void setSuit(Suit suit) {
+		this.suit = suit;
+	}
+
+	public Rank getRank() {
+		return rank;
+	}
+
+	public void setRank(Rank rank) {
+		this.rank = rank;
+	}
+	
+	
+}
