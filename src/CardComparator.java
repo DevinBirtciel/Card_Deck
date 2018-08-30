36b100def74b0944a360da5d4d527b34f40c5233
devinb <dmb1@email.arizona.edu>
2018-08-29 17:27:38 -0700

    wrapping up initial comparator implementation

--- src/CardComparator.java
+++ src/CardComparator.java
@@ -9,9 +9,12 @@ public class CardComparator implements Comparator<Card>{
 		if(!card1Suit.equals(card2Suit)) {
 			return card1Suit.getValue() - card2Suit.getValue();
 		} else {
-			
+			if(card1Suit.equals(Suit.HEART) || card1Suit.equals(Suit.CLUB)) {
+				return card1.getRank().getValue1() - card2.getRank().getValue1();
+			} else {
+				return card1.getRank().getValue2() - card2.getRank().getValue2();
+			}
 		}
-		return 0;
 	}
 
 }
--- src/Rank.java
+++ src/Rank.java
@@ -1,4 +1,22 @@
 
 public enum Rank {
-	ACE, ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING
+	ACE(0,14), ONE(1,1), TWO(2,2), THREE(3,3), 
+	FOUR(4,4), FIVE(5,5), SIX(6,6), SEVEN(7,7), EIGHT(8,8), 
+	NINE(9,9), TEN(10,10), JACK(11,11), QUEEN(12,12), KING(13,13);
+	
+	private final int value;
+	private final int value2;
+	
+	Rank(final int newValue, final int newValue2) {
+		value = newValue;
+		value2 = newValue2;
+	}
+	
+	public int getValue1() {
+		return value;
+	}
+	
+	public int getValue2() {
+		return value2;
+	}
 }
